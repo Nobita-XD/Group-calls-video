99d840c2de72fa832abde934c87f7bcbe3faf295
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-08 14:52:28 +0530

    Update piped_stream.py

--- lib/driver/piped_stream.py
+++ lib/driver/piped_stream.py
@@ -20,6 +20,18 @@ async def play_video(client, message):
     text = message.text.split(None, 2)[1:]
     user = message.from_user.mention
     chat_id = int(message.chat.title) if flags == "channel" else message.chat.id
+    try:
+        if text[0] == "channel":
+            chat_id = int(message.chat.title)
+            try:
+                input = " ".join(text[1:])
+            except Exception:
+                pass
+        else:
+            chat_id = message.chat.id
+            input = " ".join(message.command[1:])
+    except Exception:
+        pass
     if not replied:
         try:
             add_chat(str(chat_id))
